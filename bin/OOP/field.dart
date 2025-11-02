7ab76f2337a692ea12c70be6040347cc0ec64255
kristianadera76-pixel <kristianadera76@gmail.com>
2025-11-02 16:09:54 +0700

    OOP

---
+++ bin/OOP/field.dart
@@ -0,0 +1,13 @@
+class Person {
+  String name = 'Kristiana Dera';
+  String? address;
+  final country = 'Indonesia';
+}
+
+void main() {
+  var person1 = Person();
+  Person person2 = Person();
+
+  print(person1);
+  print(person2);
+}
