455eaf521ab13a1ecaed14a8ef5937e829dc6862
kristianadera76-pixel <kristianadera76@gmail.com>
2025-11-11 13:45:00 +0700

    OOP

---
+++ bin/OOP/variable_shadowing.dart
@@ -0,0 +1,10 @@
+class Person {
+  String name = "guest";
+  String? address;
+  final String country = 'Indonesia';
+
+  Person(String name, String address) {
+    this.name = name;
+    this.address = address;
+  }
+}
